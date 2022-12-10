c05ca612975f3690e67c16dc2d467f1fd8dfded5
Sergey <zseserg@mail.ru>
2022-12-10 13:45:10 +0400

    cikli

--- script.js
+++ script.js
@@ -5,12 +5,9 @@ let formatDate;
 
 
 //диалоговое окно для ввода бюджета
-monthBudget = prompt("Введите число, равное Вашему месячному бюджету" , "");
-
-alert("aaa"+monthBudget);
-
+  monthBudget = prompt("Введите число, равное Вашему месячному бюджету" , "");
 //диалоговое окно для ввода даты
-formatDate = prompt("Введите дату в формате YYYY-MM-DD" , "");
+  //formatDate = prompt("Введите дату в формате YYYY-MM-DD" , "");
 
 //бюджет appMonthBudget
 //время  appFormatDate
@@ -29,28 +26,60 @@ let appData = {
  }
 
 //бязательная статья расходов в этом месяце monthExpenses
-
-let monthExpenses1, monthExpenses2;
+let monthExpenses;
 
 //во сколько обойдётся money
+let money;
 
-let money1, money2;
 
-money1 = prompt("Во сколько обойдётся?", "" );
-monthExpenses1 = prompt("Введите обязательную статью расходов в этом месяце", "" );
+do {
 
-money2 = prompt("Во сколько обойдётся?", "" );
-monthExpenses2 = prompt("Введите обязательную статью расходов в этом месяце", "" );
+        monthExpenses = prompt("52 Введите обозначение для обязательной статьи расходов в этом месяце или нажмите ОТМЕНА для завершения цикла", "" );
+        
+        if (monthExpenses === null) {alert("55 Ввод завершён!"); break;}
+     
+        if (monthExpenses === "" ) {alert("57 Введите правильное обозначение для обязательной статьи расходов  "); continue;}
+      
+        if (monthExpenses.length > 10) {alert("59 Значение слишком длинное. Введите заново!");continue;}
+ 
+        money =Number(prompt(66+" Затраты на обязательную статью расхордов", "" ));
 
-    //заполняем свойство  appNonbidingExpenses
+        if (money === null) {alert(j+5+" Ввод завершён!"); break;}
+       
+        if (money === 0) {alert(j+7+1+" Введите правильное значение для затрат на обязательную статью расхордов!"); continue;}
 
-    appData.appObligatoryExpenses.a1 = money1;
+        if (typeof(money) == NaN) {alert(j+11+1+" Значение не цифра. Введите заново!"); continue;}
 
-    appData.appObligatoryExpenses.a3 = money2;
+        //заполняем свойство  appNonbidingExpenses
+        appData.appObligatoryExpenses[monthExpenses] = money;
+       // alert("1 monthExpenses="+monthExpenses+"  "+appData.appObligatoryExpenses['aa']);
+         
+} while (true);
 
-    alert("бюджет на день = "+ (appData.appMonthBudget / 30));
 
-    //console.log(appData.appObligatoryExpenses.a3);
+
+   alert("Цикл ввода статей расходов завершёнзавершён!");
+
+/*
+for (let i=1; i<3; i++) {
+
+}
+
+let j=1;
+while (j<3) {
+
+    j++;
+}
+*/
+
+
+
+   alert("бюджет на день = "+ (appData.appMonthBudget / 30));
+
+  
+    console.log(appData.appObligatoryExpenses[0]);
+    console.log(appData.appObligatoryExpenses['b']);
+  
 
 
 
